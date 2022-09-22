2ad62bc1b7f00a3fed3b75f4714a5b31078f412a
1337-Codes <78195003+1337-Codes@users.noreply.github.com>
2022-09-22 17:42:51 +0400

    Add files via upload

--- Division Game Engine/source/Aspect2D.py
+++ Division Game Engine/source/Aspect2D.py
@@ -3,19 +3,21 @@ import pygame
 class x:
     def translate(img, dx):
         pass
-    def rotate(img, dx):
-        pass
     def scale(img, dx):
         pass
-    def initialState(img, xpos, xrot, x):
+    def initialState(img, xpos, x):
         pass
 
 class y:
     def translate(img, dy):
         pass
-    def rotate(img, dy):
-        pass
     def scale(img, dy):
         pass
-    def initialState(img, ypos, yrot, y):
+    def initialState(img, ypos, y):
+        pass
+
+class t:
+    def rotate(img, dt):
+        pass
+    def initialState(img, t):
         pass
